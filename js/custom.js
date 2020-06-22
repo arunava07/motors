c78e61f9902ed120e8f7d837d0c8314d49a69bcc
d.arunava07 <d.arunava07@gmail.com>
2020-06-22 11:35:19 +0530

    any text

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$('.c-banner-wr').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+   prevArrow: null,
+   nextArrow: null,
+   speed: 500,
+  prevArrow: null,
+  nextArrow: null,
+  
+});
+$(document).ready(function(){
+$('.select2').select2();
+
+});
